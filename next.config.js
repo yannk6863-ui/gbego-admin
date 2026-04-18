73e00761f79f3389b980630be7ade4dc07eae098
yannk6863-ui <yannk6863@gmail.com>
2026-04-18 00:50:32 +0000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ assertAdminEnv();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ['http://localhost:3001', 'http://127.0.0.1:3001', 'http://192.168.1.17:3001'],
   outputFileTracingRoot: path.resolve(__dirname),
   images: {
     unoptimized: true,
